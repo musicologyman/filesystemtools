2beaebac6be83dae7615a48455c79a8002113322
Richard Wattenbarger <github@wattenbarger.net>
2023-01-18 07:56:27 -0500

    refactoring

--- get_extensions.py
+++ get_extensions.py
@@ -19,20 +19,28 @@ def get_extensions_set(parent: Path, recursive: bool=True) -> Iterable[str]:
     return pipe(get_extensions(parent, recursive),
                 set,
                 partial(sorted, key=str.lower))
-    
-def main():
-    INDENT = ' ' * 2
+
+def setup_command_line() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument('top_level_dir', type=Path)
     parser.add_argument('-r', '--recursive', action='store_true')
-    args : Namespace = parser.parse_args()
+    return parser.parse_args()
 
-    print('Extensions:')
-    for ext in get_extensions_set(args.top_level_dir, recursive=args.recursive):
+def print_extensions(extensions: Iterable[str]) -> None:
+    INDENT = ' ' * 2
+    for ext in extensions:
         if not ext:
             print(f'{INDENT}(no extension)')
         else:
             print(f'{INDENT}{ext}')
+
+def main():
+    args : Namespace = setup_command_line()
+
+    print('Extensions:')
+    top_level_dir = args.top_level_dir.expanduser()
+    pipe(get_extensions_set(top_level_dir, recursive=args.recursive),
+         print_extensions)
     
 if __name__ == '__main__':
     main()
